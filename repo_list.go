4d35574a9e2390d6e24657fa049d35d22315018a
Sascha Andres <sascha.andres@outlook.com>
2018-06-02 09:53:25 +0200

    feat: implement list repositories

--- repo_list.go
+++ repo_list.go
@@ -1,7 +1,51 @@
 package engine
 
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+
+	yaml "gopkg.in/yaml.v1"
+)
+
 // ListRepositories lists all repositories downloaded
 func (e *Engine) ListRepositories() ([]*Repository, error) {
-	e.logger.Info(e.storageDirectory)
-	return nil, nil
+	logger := e.logger.WithField("method", "ListRepositories")
+	result := make([]*Repository, 0)
+	entries, err := ioutil.ReadDir(e.storageDirectory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			fullPath := path.Join(e.storageDirectory, entry.Name())
+			logger.Infof("looking in [%s]", fullPath)
+			if _, err := os.Stat(path.Join(fullPath, ".go-template.yml")); err == nil {
+				logger.Info("found .go-template.yml")
+				f, err := os.Open(path.Join(fullPath, ".go-template.yml"))
+				if err != nil {
+					return nil, err
+				}
+				defer func() {
+					if err := f.Close(); err != nil {
+						logger.Error(err.Error())
+					}
+				}()
+				content, err := ioutil.ReadAll(f)
+				if err != nil {
+					return nil, err
+				}
+				var templateFile TemplateFile
+				err = yaml.Unmarshal(content, &templateFile)
+				if err != nil {
+					return nil, err
+				}
+				result = append(result, &templateFile.Repository)
+			} else {
+				logger.Warnf("no template in [%s]", fullPath)
+			}
+		}
+	}
+	return result, nil
 }
--- type.go
+++ type.go
@@ -9,12 +9,34 @@ type (
 		logger           *logrus.Entry
 	}
 
+	// TemplateFile contains all relevant information about a template
+	TemplateFile struct {
+		Repository     Repository      `yaml:"project"`                  // Repository information
+		Transformation *Transformation `yaml:"transformation,omitempty"` // Transformation instructions
+		InitializeGit  bool            `yaml:"git"`                      // If true new project will be placed in a new git repository
+	}
+
 	// Repository contains information about a repository downloaded
 	Repository struct {
 		Name        string // Name (given) of repository
 		Description string // Description provided by author
 		Author      string // Author provided self
 		URL         string // URL to remote git repository or homepage
+	}
+
+	// Transformation contains information how to transform the template
+	Transformation struct {
+		ExcludedFiles []string            `yaml:"excluded-files"`         // ExcludedFiles may contain a list of files to not include in the new project
+		Renames       []FromToInformation `yaml:"renames,omitempty"`      // Renames is a list of renames in the filesystem, type may be directory or file or nothing
+		Replacements  []FromToInformation `yaml:"replacements,omitempty"` // Replacements is a list of replacements within the files, type may be a filename matching regex
+		// TODO Templates     []string `yaml:"templates"`      // Templates is a list of files to handle using text/template
+	}
+
+	// FromToInformation contains instruction how to change the source
+	FromToInformation struct {
+		From string  `yaml:"from"`           // From is the part to change
+		To   string  `yaml:"to"`             // To is the destination and is treated with text/template
+		Type *string `yaml:"type,omitempty"` // Type may be used as an indicator
 	}
 )
 
