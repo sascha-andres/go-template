b284d4276b890312f942a19357e1993417543389
Sascha Andres <sascha.andres@outlook.com>
2018-06-06 13:06:58 +0200

    feat: simplify code

--- repo_info.go
+++ repo_info.go
@@ -16,12 +16,10 @@ package engine
 
 import "path"
 
+// InfoRepository returns information about a repository
 func (e *Engine) InfoRepository(name string) (*Repository, []string, error) {
 	if ok, err := e.exists(path.Join(name)); err != nil || !ok {
-		if err != nil {
-			return nil, nil, err
-		}
-		return nil, nil, nil
+		return nil, nil, err
 	}
 	templateFile, err := e.readTemplateFile(path.Join(e.storageDirectory, name, ".go-template.yml"))
 	if err != nil {
